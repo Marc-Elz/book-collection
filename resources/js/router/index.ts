251b10e660fcb565a872186ab75e27f482a314b5
Marc-Elz <marcelzinga@live.nl>
2026-05-06 15:09:32 +0200

    minor formatchange

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { bookRoutes } from '../domains/books/routes';
+import { createRouter, createWebHistory } from "vue-router";
+import { bookRoutes } from "../domains/books/routes";
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [...bookRoutes],
-})
+});
