d754cc509500a535901647a78f5adabc6c5021eb
Marc-Elz <marcelzinga@live.nl>
2026-05-11 15:26:20 +0200

    Added all crud methods to factory actions

--- resources/js/services/store/index.ts
+++ resources/js/services/store/index.ts
@@ -1,16 +1,22 @@
 import { ref, computed } from 'vue';
-import { getRequest } from '../http';
+import { getRequest, postRequest, putRequest, deleteRequest} from '../http';
 
 export const storeModuleFactory = (moduleName: any) => {
     const state = ref<any>({});
 
     const getters = {
-        all: computed(() => state.value)
-    }
+        all: computed(() => state.value),
+        
+        getById: (id) => computed(() => state.value[id])
+    };
 
     const setters = {
         setAll: (items:any) => {
             for (const item of items) state.value[item.id] = Object.freeze(item);
+        },
+
+        deleteByItem: (item) => {
+            delete state.value[item.id];
         }
     };
 
@@ -19,7 +25,26 @@ export const storeModuleFactory = (moduleName: any) => {
             const { data } = await getRequest(moduleName);
             if (!data) return;
             setters.setAll(data);
+        },
+
+        create: async (item) => {
+            const { data } = await postRequest(moduleName, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+
+        update: async (id, item) => {
+            const { data } = await putRequest(`${moduleName}/${id}`, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+
+        delete: async (id) => {
+            await deleteRequest(`${moduleName}/${id}`);
+            setters.deleteByItem({ id })
         }
+
+
     };
 
     return { getters, setters, actions };
