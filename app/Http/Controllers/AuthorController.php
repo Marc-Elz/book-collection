ccaca842a3893a15fe1876def758d2daf43e353a
Marc-Elz <marcelzinga@live.nl>
2026-05-13 10:14:52 +0200

    Fix: 500 instead of expected 422

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use App\Models\Author;
 use App\Models\Book;
 use App\Http\Resources\AuthorResource;
