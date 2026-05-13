4bbf9502d47e360616be1ccef3728dff7efc44e5
Marc-Elz <marcelzinga@live.nl>
2026-05-13 10:15:29 +0200

    Autoformat + debugging

--- resources/js/domains/authors/store.ts
+++ resources/js/domains/authors/store.ts
@@ -1,36 +1,35 @@
-import { storeModuleFactory } from '../../services/store';
-
+import { storeModuleFactory } from "../../services/store";
 
 export interface authorType {
     id: number;
     name: string;
 }
 
-const authorStore = storeModuleFactory('authors');
-
+const authorStore = storeModuleFactory("authors");
 
 export const getAllAuthors = authorStore.getters.all;
-export const getAuthorById =  authorStore.getters.getById;
-
+export const getAuthorById = authorStore.getters.getById;
 
 export const fetchAuthors = async () => {
     return await authorStore.actions.getAll();
 };
 
-export const createAuthor = async (newAuthor:authorType) => {
+export const createAuthor = async (newAuthor: authorType) => {
     return authorStore.actions.create(newAuthor);
-}
+};
 
-export const updateAuthor = async (id:number|string|string[], updatedAuthor:authorType) => {
-    return await authorStore.actions.update(id, updatedAuthor)
-}
+export const updateAuthor = async (
+    id: number | string | string[],
+    updatedAuthor: authorType,
+) => {
+    return await authorStore.actions.update(id, updatedAuthor);
+};
 
-export const deleteAuthor = async (id:number) => {
+export const deleteAuthor = async (id: number) => {
     try {
         return await authorStore.actions.delete(id);
-    }catch(error: any){
-        console.error('Error deleting author');
-        console.error(error.response.value);
+    } catch (error: any) {
+        console.error("Error deleting author");
         throw error;
     }
-}
+};
