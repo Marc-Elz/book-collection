17a33aa56dc76bf5451fdf95817bb82adf59520d
Marc-Elz <marcelzinga@live.nl>
2026-05-13 10:20:07 +0200

    Autoformatting

--- resources/js/services/error/index.ts
+++ resources/js/services/error/index.ts
@@ -1,21 +1,24 @@
-import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 
 interface ErrorBag {
     [property: string]: string[];
 }
 
 const errorBag = ref<ErrorBag>({});
-const message = ref('');
+const message = ref("");
 
 export const getErrorBag = computed(() => errorBag.value);
 export const getMessage = computed(() => message.value);
 
-export const getErrorByProperty = (property: string) => computed(() => errorBag.value[property]);
+export const getErrorByProperty = (property: string) =>
+    computed(() => errorBag.value[property]);
 
-export const setErrorBag = (bag: ErrorBag) => {(errorBag.value = bag); console.log(errorBag.value)};
+export const setErrorBag = (bag: ErrorBag) => {
+    errorBag.value = bag;
+};
 export const setMessage = (newMessage: string) => {
-    message.value = newMessage || 'Er is een fout opgetreden.';
+    message.value = newMessage || "Er is een fout opgetreden.";
 };
 
 export const destroyErrors = () => (errorBag.value = {});
-export const destroyMessage = () => (message.value = '');
+export const destroyMessage = () => (message.value = "");
--- resources/js/services/http/index.ts
+++ resources/js/services/http/index.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import { destroyErrors, destroyMessage } from '../error';
-import { setErrorBag, setMessage } from '../error';
+import axios from "axios";
+import { destroyErrors, destroyMessage } from "../error";
+import { setErrorBag, setMessage } from "../error";
 
 const http = axios.create({
-    baseURL: '/api',
+    baseURL: "/api",
     headers: {
-        'Content-Type': 'application/json'
-    }
+        "Content-Type": "application/json",
+    },
 });
 
 export const getRequest = (endpoint) => http.get(endpoint);
@@ -15,21 +15,21 @@ export const putRequest = (endpoint, data) => http.put(endpoint, data);
 export const deleteRequest = (endpoint) => http.delete(endpoint);
 
 http.interceptors.request.use(
-    config => {
+    (config) => {
         destroyErrors(); // Wis oude fouten voordat een nieuw verzoek wordt uitgevoerd
         destroyMessage(); // Wis oude "messages" voordat een nieuw verzoek wordt uitgevoerd
         return config;
     },
-    error => Promise.reject(error)
+    (error) => Promise.reject(error),
 );
 
 http.interceptors.response.use(
-    response => response,
-    error => {
+    (response) => response,
+    (error) => {
         if (error.response && error.response.status === 422) {
             setErrorBag(error.response.data.errors); // Sla validatiefouten op in de error bag
             setMessage(error.response.data.message); // Sla de algemene foutmelding op
         }
         return Promise.reject(error);
-    }
+    },
 );
